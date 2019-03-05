18381bb302ae7a5c9e63f386e458784801d82c37
tcross <tom.bz2@gmail.com>
2019-03-04 21:59:44 -0600

    add documentation and an example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+Initial release is very "developer preview"-ish.  It should do everything it needs to for iOS and Android with basic documentation.
+
+TODO:
+write iOS documentation
+write unit tests
+make a basic iOS and Android Example Application
+
--- test/nearby_messages_api_test.dart
+++ test/nearby_messages_api_test.dart
@@ -5,6 +5,30 @@ import 'package:nearby_messages_api/nearby_messages_api.dart';
 void main() {
   const MethodChannel channel = MethodChannel('nearby_messages_api');
 
+  /**
+   * This method gets the message when an a nearby device sends one
+   */
+  void onFound(dynamic arguments) {
+  print(arguments);
+  }
+
+  /**
+   * This method gets the message when a nearby device leaves
+   */
+  void onLost(dynamic arguments) {
+  print(arguments);
+  }
+
+  NearbyMessagesApi nearbyMessagesApi = NearbyMessagesApi(onFound, onLost);
+
+  Future<void> main() async {
+    await nearbyMessagesApi.backgroundSubscribe();
+  }
+
+  Future<void> dispose() async {
+    await nearbyMessagesApi.backgroundUnsubscribe();
+  }
+
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
       return '42';
